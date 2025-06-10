e5885b230538da4618a36df1e0d6334a21e1158b
unknown <rajuchoudri5@gmail.com>
2025-06-10 13:40:23 +0530

    4th changess

--- MyProject/src/app/app.module.ts
+++ MyProject/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
@@ -14,7 +18,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinnerModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- MyProject/src/app/user-list/user-list.component.ts
+++ MyProject/src/app/user-list/user-list.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { User, UserService } from '../user.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit, AfterViewInit {
+
+
+   displayedColumns: string[] = ['name', 'email', 'phone', 'company'];
+  dataSource = new MatTableDataSource<User>();
+  isLoading = true;
+  error = '';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe({
+      next: (users) => {
+        this.dataSource.data = users;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to fetch users';
+        this.isLoading = false;
+      }
+    });
+  }
+
+  // applyFilter(filterValue: string) {
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
+  // }
+
+  applyFilter(event: Event) {
+  const input = event.target as HTMLInputElement;
+  const value = input.value.trim().toLowerCase();
+  this.dataSource.filter = value;
+}
+
+
+  // For case-insensitive filtering by name only
+  ngAfterViewInit() {
+    this.dataSource.filterPredicate = (data: User, filter: string) =>
+      data.name.toLowerCase().includes(filter);
+  }
+
 
 }
